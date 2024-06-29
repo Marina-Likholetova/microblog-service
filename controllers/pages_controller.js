d26e17156f4b9de6e3e44aff39586fac85183aa9
Marina <likholetova.marina@gmail.com>
2024-06-29 18:56:08 +0300

    add page controller

---
+++ controllers/pages_controller.js
@@ -0,0 +1,100 @@
+const { AuthError } = require("../errors");
+const service = require("../services");
+
+function addPageContext(req, _resp, next) {
+    const { context } = req.session || {};
+
+    req.__pageContext = {
+        isLoggedIn: !!context?.role,
+        ...context,
+    };
+
+    next();
+}
+
+function checkAuth(req, _resp, next) {
+    try {
+        const { userId, isLoggedIn } = req.__pageContext;
+
+        if (!userId || !isLoggedIn) throw new AuthError({ msg: "No authorized user found" });
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+function renderPage(templateName) {
+    return (req, resp) => {
+        resp.render(templateName, req.__pageContext);
+    };
+}
+
+async function getAllPosts(req, _resp, next) {
+    const postsList = await service.post.getAllPosts();
+
+    req.__pageContext.postsList = postsList;
+
+    next();
+}
+
+async function createNewPost(req, resp, next) {
+    try {
+        const { userId } = req.__pageContext;
+
+        await service.post.createNewPost({ ...req.body, authorId: userId });
+
+        resp.redirect(req.baseUrl);
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function getUserPosts(req, _resp, next) {
+    try {
+        const { userId } = req.__pageContext;
+
+        const postsList = await service.post.getUserPosts(userId);
+        req.__pageContext.postsList = postsList;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function getPostById(req, _resp, next) {
+    try {
+        const { postId } = req.params;
+
+        const post = await service.post.getPostById(postId);
+        req.__pageContext.post = post;
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function createNewComment(req, resp, next) {
+    try {
+        const { userId } = req.__pageContext;
+
+        await service.comment.createNewComment({ ...req.body, authorId: userId });
+
+        resp.redirect("back");
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    addPageContext,
+    renderPage,
+    getAllPosts,
+    getUserPosts,
+    getPostById,
+    createNewPost,
+    createNewComment,
+    checkAuth,
+};
